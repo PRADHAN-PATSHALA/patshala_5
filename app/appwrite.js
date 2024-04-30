291ad001f43e7b6e205cfd5fe77792b29613de4d
soham <pradhanpatshala@gmail.com>
2024-04-30 13:26:13 +0530

    jjkl

--- app/appwrite.js
+++ app/appwrite.js
@@ -5,7 +5,7 @@ import { Client, Account} from 'appwrite';
    export const client = new Client;
 
    client
-    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setEndpoint('https://pradhanpatshalanazat0.netlify.app/') 
     .setProject('663056c30027d48154ae')
 
    export const account = new Account(client);
