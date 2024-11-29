65f956938df090edbdbc5e9273b0f58310dfa412
soham <pradhanpatshala@gmail.com>
2024-11-30 03:51:57 +0530

    ff

--- app/component/Hero.js
+++ app/component/Hero.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <>
-      <div className="h-full w-full relative text-md sm:text-6xl">
+      <div className="h-full w-full relative text-lg sm:text-6xl">
         <div className="h-[30vh] w-[30vh] shadow-[0_40px_80px_-15px_cyan] bg-cyan-400 absolute -left-[10vh] -top-[10vh] rounded-full z-[-1]"></div>
         <h1 className="font-serif font-light absolute top-[40vh] -translate-x-[50%] left-[50%]">Pradhan Pathshala</h1>
         <p className="font-sans tracking-[.5vh] absolute top-[50vh] text-sm sm:right-[30vh] right-[3vh] text-cyan-800">PRADHAN <br />COACHING <br />CENTER</p>
