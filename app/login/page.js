1d82caf8fd5e9bea49479ba3e82afe3346950539
soham <pradhanpatshala@gmail.com>
2024-12-02 09:29:43 +0530

    all done

--- app/login/page.js
+++ app/login/page.js
@@ -25,8 +25,8 @@ const login = () => {
   return (
     <>
       <form onSubmit={handler} className="min-h-screen w-full bg-black">
-      <label className="relative text-white border-cyan-200 border-solid border-[.5px] mb-3 rounded-lg h-10 w-full items-center top-[10vh] flex text-lg font-serif font-semibold">Name: <input type="name" placeholder="Type the student name" className="font-sans font-light text-md text-white w-[70vh] bg-[#111]  rounded-lg -translate-x-[50%] left-[50%] absolute" onChange={(e) => { setName(e.target.value) }} /></label>
-      <label className="relative text-white border-cyan-200 border-solid border-[.5px] mb-3 rounded-lg h-10 w-full items-center top-[10vh] flex text-lg font-serif font-semibold">Password: <input type="password" placeholder="Type the student father's name" className="font-sans font-light text-md text-white w-[70vh] bg-[#111]  rounded-lg -translate-x-[50%] left-[50%] absolute" onChange={(e) => { setPassword(e.target.value) }} /></label>
+      <label className="relative text-white border-cyan-200 border-solid border-[.5px] mb-3 rounded-lg h-10 w-full items-center top-[10vh] flex text-lg font-serif font-semibold">Name: <input type="name" placeholder="Type the name" className="font-sans font-light text-md text-white w-[70vh] bg-[#111]  rounded-lg -translate-x-[50%] left-[50%] absolute" onChange={(e) => { setName(e.target.value) }} /></label>
+      <label className="relative text-white border-cyan-200 border-solid border-[.5px] mb-3 rounded-lg h-10 w-full items-center top-[10vh] flex text-lg font-serif font-semibold">Password: <input type="password" placeholder="Type the password" className="font-sans font-light text-md text-white w-[70vh] bg-[#111]  rounded-lg -translate-x-[50%] left-[50%] absolute" onChange={(e) => { setPassword(e.target.value) }} /></label>
       <input className="px-8 py-2 bg-cyan-400 text-white -translate-x-[50%] left-[50%] absolute top-[40vh] rounded-full cursor-pointer" type="submit" />
       </form>
     </>
